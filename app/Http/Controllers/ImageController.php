daf01c797337d1210b4d8319023da37dd47a0b28
DeFreitas-Samuel <defreitas.samuel.99@gmail.com>
2023-05-03 14:03:16 -0400

    improved the way that images are served

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,27 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use http\Env\Response;
-
 class ImageController
 {
     public function show($name){
-        if( !file_exists('C:/Users/defre/source/repos/PhoneEcommerceAPI/public/images/'.$name)){
+        $url = $_SERVER['DOCUMENT_ROOT'] . "\\images\\" . $name;
+        if( !file_exists($url)){
+            
             return response()->noContent(404);
         }
 
-        return response()->file('C:/Users/defre/source/repos/PhoneEcommerceAPI/public/images/'.$name);
+        return response()->file($url);
     }
 
-    public function test(){
-        for ($index = 1; $index < 61; $index++){
-            $product = Product::find($index);
-            $product->imageUrl=  "http://localhost:8000/api/images/" . $index . ".jpg";
-            $product->save();
-        }
-
-        return response()->noContent(200);
-
-    }
 }
