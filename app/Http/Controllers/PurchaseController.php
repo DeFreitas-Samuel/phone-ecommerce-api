e397a42c9c17d96241df68e8f106fb895bdb3e88
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2023-06-11 01:45:04 -0400

    Made the purchase a transaction so if if fails the purchase is not saved uncomplete

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -12,95 +12,107 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
 use App\Models\User;
 use Carbon\Carbon;
-use Error;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Exception;
 
 class PurchaseController extends Controller
 {
     public function store(Request $request)
     {
+        try {
+            DB::beginTransaction();
 
-        $order = $request->all();
-        $totalFromOrder = $order['total'];
-        $userIdFromOrder = $order['user_id'];
-        $paymentMethod = $order['payment_method'];
-        $last4DigitsOfCard = $order['last4DigitsOfCard'];
-        $boughtProductsArray = $order['products'];
-        $shippingAddress = $order['shipping_address'];
+            $order = $request->all();
+            $totalFromOrder = $order['total'];
+            $userIdFromOrder = $order['user_id'];
+            $paymentMethod = $order['payment_method'];
+            $last4DigitsOfCard = $order['last4DigitsOfCard'];
+            $boughtProductsArray = $order['products'];
+            $shippingAddress = $order['shipping_address'];
 
-        $paymentMethodUsed = PaymentMethod::where('name', $paymentMethod)->firstOrFail();
-
-
-        $bill = new Bill();
-        $bill->total = $totalFromOrder;
-        if(Auth::id() !== $userIdFromOrder){
-            return response()->json(['error_message'=> 'The user that is making the request is not the same as the one that is authenticated'],409);
-        }
-        $user = User::find(Auth::id());
-        $bill->user()->associate($user);
-
-        if($paymentMethodUsed->name === 'Debit/Credit Card'){
-            $bill->amount_due = 0;
-        }
-        else{
-            $bill->amount_due = $totalFromOrder;
-        }
-          
-        $bill->save();
-
-        foreach ($boughtProductsArray as $product) {
-            $billLine = new BillLine();
-            $productBought = Product::find($product["product_id"]);
-            $calculatedPrice = round(($productBought->price * $product["quantity"]),2);
+            $paymentMethodUsed = PaymentMethod::where('name', $paymentMethod)->firstOrFail();
 
 
-            $billLine->product()->associate($productBought);
+            $bill = new Bill();
+            $bill->total = $totalFromOrder;
+            if (Auth::id() !== $userIdFromOrder) {
+                return response()->json(['error_message' => 'The user that is making the request is not the same as the one that is authenticated'], 409);
+            }
+            $user = User::find(Auth::id());
+            $bill->user()->associate($user);
 
-            if($product["totalPrice"] !== $calculatedPrice){
-                throw new PriceMismatchException("The price calculated differs from what it was received in the front end");
+            if ($paymentMethodUsed->name === 'Debit/Credit Card') {
+                $bill->amount_due = 0;
+            } else {
+                $bill->amount_due = $totalFromOrder;
+            }
+
+            $bill->save();
+
+            foreach ($boughtProductsArray as $product) {
+                $billLine = new BillLine();
+                $productBought = Product::find($product["product_id"]);
+                $calculatedPrice = round(($productBought->price * $product["quantity"]), 2);
+
+
+                $billLine->product()->associate($productBought);
+
+                if ($product["totalPrice"] !== $calculatedPrice) {
+                    throw new PriceMismatchException("The price calculated differs from what it was received in the front end");
+                }
+
+
+                $billLine->price = $product["totalPrice"];
+                $billLine->quantity = $product["quantity"];
+                $billLine->description = $product["name"];
+                $billLine->bill()->associate($bill);
+
+                $billLine->save();
             }
 
 
-            $billLine->price = $product["totalPrice"];
-            $billLine->quantity = $product["quantity"];
-            $billLine->description = $product["name"];
-            $billLine->bill()->associate($bill);
 
-            $billLine->save();
+
+
+            if ($paymentMethodUsed->name === 'Debit/Credit Card') {
+                $payment = new Payment();
+                $payment->bill()->associate($bill);
+                $payment->user()->associate($user);
+                $payment->paymentMethod()->associate($paymentMethodUsed);
+                $payment->amount = $totalFromOrder;
+                $payment->last_4_digits_of_card = $last4DigitsOfCard;
+                $payment->save();
+            }
+
+
+            $shipment = new Shipment();
+            $shipment->bill()->associate($bill);
+            $shipment->user()->associate($user);
+            $shipment->address = $shippingAddress['address'];
+            $shipment->city = $shippingAddress['city'];
+            $shipment->state = $shippingAddress['state'];
+            $shipment->phone_number = $shippingAddress['phoneNumber'];
+            $shipment->zipcode = $shippingAddress['zipCode'];
+            $shipment->estimated_arrival = Carbon::now()->addDays(7);
+            $shipment->delivered = 0;
+
+            $shipment->save();
+
+            $billLines = BillLine::where('bill_id', $bill->id)->get();
+
+            DB::commit();
+
+            return response()->json([
+                'requestAll' => $request->all(),  'bill' => $bill, 'shipment' => $shipment, 'billLines' => $billLines,
+                'last4DigitsOfCard' => $last4DigitsOfCard, 'paymentMethod' => $paymentMethod
+            ], 200);
+        } catch (Exception $e) {
+            DB::rollBack();
+            report($e);
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-
-
-
-        
-        if($paymentMethodUsed->name === 'Debit/Credit Card'){
-            $payment = new Payment();
-            $payment->bill()->associate($bill);
-            $payment->user()->associate($user);
-            $payment->paymentMethod()->associate($paymentMethodUsed);
-            $payment->amount = $totalFromOrder;
-            $payment->last_4_digits_of_card = $last4DigitsOfCard;
-            $payment->save();
-        }
-
-
-        $shipment = new Shipment();
-        $shipment->bill()->associate($bill);
-        $shipment->user()->associate($user);
-        $shipment->address = $shippingAddress['address'];
-        $shipment->city = $shippingAddress['city'];
-        $shipment->state = $shippingAddress['state'];
-        $shipment->phone_number = $shippingAddress['phoneNumber'];
-        $shipment->zipcode = $shippingAddress['zipCode'];
-        $shipment->estimated_arrival = Carbon::now()->addDays(7);
-        $shipment->delivered = 0;
-
-        $shipment->save();
-
-        $billLines = BillLine::where('bill_id', $bill->id)->get();
-      
-        return response()->json(['requestAll' => $request->all(),  'bill' => $bill,  'billLine' => $billLine, 'shipment' => $shipment, 'billLines'=>$billLines,
-                                'last4DigitsOfCard' => $last4DigitsOfCard, 'paymentMethod' => $paymentMethod],200);
-
     }
 }
